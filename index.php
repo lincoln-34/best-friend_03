890fa1b22aca8dd1dc595a4aa107c2ebaf21256d
lincoln-34 <lincolnload210@gmail.com>
2026-01-07 16:27:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -40,12 +40,12 @@
 
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top navbar-scrolled">
     <div class="container ps-5">
-      <a class="navbar-brand" href="index.html">BestFriend 💙</a>
+      <a class="navbar-brand" href="index.php">BestFriend 💙</a>
     </div>
     <div class="collapse navbar-collapse pe-5" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link active" href="index.html">Beranda</a>
+          <a class="nav-link active" href="index.php">Beranda</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="tentang-aku.php">Tentang</a>
@@ -138,3 +138,4 @@
 
 </body>
 </html>
+
